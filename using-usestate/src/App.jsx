d90f9a4d264afb9d1051d81b7a6aa2b204c320c5
Christopher Vigliotti <christopher@christophervigliotti.com>
2026-03-27 19:50:04 -0400

    add works, edit works...but delete does not

--- using-usestate/src/App.jsx
+++ using-usestate/src/App.jsx
@@ -1,22 +1,12 @@
 import * as React from "react"
 
-// 8 adding todolist component (as well as adding default function App at the bottom of the page)
 function TodoList() {
-  /*
-  const [todo, setTodo] = React.useState({
-    id: 1,
-    label: "Learn React",
-    completed: false,
-  }) */
   const [todos, setTodos] = React.useState([
     { id: 1, label: "Learn React", completed: false },
     { id: 2, label: "Learn Next.js", completed: false },
     { id: 3, label: "Learn to yodel", completed: false }
   ])
 
-  /* 8 Whenever you're in a situation like this, what you'll want to do is create a function in the component where the state is located, and then pass that function down via props.
-
-  Then, you can pass data back up to the parent component by passing an argument to the function you passed down. */
   const handleUpdateTodo = (updatedTodo) => {
     const newTodos = todos.map((todo) =>
       todo.id === updatedTodo.id ? updatedTodo : todo
@@ -36,6 +26,7 @@ function TodoList() {
 
   return (
    <ul>
+      <TodoComposer handleAddTodo={handleAddTodo} />
       {todos.map((todo) => (
         <Todo
           key={todo.id}
@@ -45,40 +36,57 @@ function TodoList() {
         />
       ))}
     </ul>
-    /*
-    <ul>
-      <Todo
-        todo={todo}
-        handleUpdateTodo={handleUpdateTodo} // 8 (continued)
-      />
-    </ul>
-    */
   )
 }
 
-/*
-function Todo () {
-  const [todo, setTodo] = React.useState({ // 6 representing our todo item as an object (as opposed to single state values)
-    id: 1,
-    label: "Learn React",
-    completed: false, // is that really right, I can have a comma at the end?
-  })
-    9 replacing this with...*/
+function createTodo (label) {
+  return {
+    id: Math.floor(Math.random() * 10000),
+    label,
+    completed: false,
+  }
+}
+
+function TodoComposer({ handleAddTodo }) {
+  const [label, setLabel] = React.useState("")
+
+  const handleUpdateLabel = (e) => setLabel(e.target.value)
+
+  const handleAddTodoClick = () => {
+    const todo = createTodo(label)
+    handleAddTodo(todo)
+    setLabel("")
+  }
+
+  return (
+    <li>
+      <input
+        placeholder="Add a new todo"
+        type="text"
+        value={label}
+        onChange={handleUpdateLabel}
+      />
+    <button
+      disabled={label.length === 0}
+      onClick={handleAddTodoClick}
+    >
+      Add Todo
+    </button>
+    </li>
+  )
+}
+
 function Todo ({todo, handleUpdateTodo}){
 
   const [label, setLabel] = React.useState("Learn React") // 3 adding 'edit single todo item' here
   const [completed, setCompleted] = React.useState(false) // 2 state
   const [editing, setEditing] = React.useState(false) // 4 adding state to keep track of is user editing
 
-  /* 7 updating these two event handlers to handle todo state as an object rather than individual pieces of state
-  const handleCheckboxClick = () => setCompleted(!completed) // 2 event handler (is hooked to the prop below)
-  const handleUpdateLabel = (e) => setLabel(e.target.value) // 5 new handler for changing the label on our state */
   const handleCheckboxClick = () => handleUpdateTodo({
     ...todo,
     completed: !todo.completed,
   })    
 
-  // const handleEditClick = () => setEditing(!editing) // 4 adding method that toggled the new editing state...
   const handleEditClick = () => handleDeleteTodo(todo.id)
 
   const handleEditTodo = (e) => handleUpdateTodo({
@@ -87,24 +95,14 @@ function Todo ({todo, handleUpdateTodo}){
   })
 
   const handleDeleteClick = () => handleDeleteTodo(todo.id)
-
-  /*
-  const handleUpdateLabel = (e) => handleUpdateTodo({
-    ...todo,
-    label: e.target.value
-  })  
-  */
-
   
   return (
     <div>
-      {/* hey look this is how you format comments within JSX */}
       <label htmlFor={todo.id}> {/* htmlFor was checkbox*/}
         <div>
           <input
             type="checkbox"
             id="{todo.id}" // id was checkbox
-            // 6 changed this checked={completed} to this...
             checked={todo.completed}
             onChange={handleCheckboxClick} // 2 this prop (is hooked to the event handler)
           />
@@ -113,18 +111,14 @@ function Todo ({todo, handleUpdateTodo}){
         {editing === true ? (
           <input 
             type="text" 
-            // 6 changed this value={label} to...
             value={todo.label}
             onChange={handleUpdateLabel} // 5 (continued)
           />
         ) : (
           <span>{todo.label}</span>/* 6 was <span>{label}</span> */
-
         )}
 
       </label>
-      {/* 4 ...& hook it to this button */}
-      {/*                   v v v v v v */}
       <button onClick={handleEditClick}> 
         {editing ? "Save" : "Edit"}
       </button>
